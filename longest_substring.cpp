22d79a48929ec19f32f27a7e907696d0f2394aa0
kjjosdj <kjjosdj@fedora.com>
2023-09-28 11:35:54 +0530

    more cleaner

--- longest_substring.cpp
+++ longest_substring.cpp
@@ -3,18 +3,11 @@
 #include <iostream>
 
 using namespace std;
-#define arraysize(array) (sizeof(array) / sizeof(*array))
 
+// This code is gone optimize
+#define GETBIT64(x, y) (x[y % 64] >> (y % 64) & (int64_t)1)
+#define SETBIT64(x, y) (x[y % 64] |= ((int64_t)1 << (y % 64)))
 
-#define GETBIT(x, y) ((x >> y) & (int64_t)1)
-#define SETBIT(x, y) (x |= ((int64_t)1 << y))
-
-struct checkbox{
-	int64_t first, second;
-};
-
-#define SETBIT128(x, y) (y >= 64 ? SETBIT(x.first, y-64): SETBIT(x.second, y))
-#define GETBIT128(x, y) (y >= 64 ? GETBIT(x.first, y-64): GETBIT(x.second, y))
 
 class Solution {
     public:
@@ -29,13 +22,13 @@ class Solution {
 	int idx, idy, result = 0;
 
 	for (idx = 0; idx < s.size(); idx++) {
-		checkbox checkboxinfo = { 0 };
-		SETBIT128(checkboxinfo, (int)s[idx]);
+		int64_t checkbox[2] = { 0 };
+		SETBIT64(checkbox, (int)s[idx]);
 		for (idy = idx+1; idy < s.size(); idy++) {
-			if (GETBIT128(checkboxinfo, (int)s[idy])) {
+			if (GETBIT64(checkbox, (int)s[idy])) {
 				break;
 			}
-			SETBIT128(checkboxinfo, (int)s[idy]);
+			SETBIT64(checkbox, (int)s[idy]);
 		}
 		result = max(result, idy-idx);
 	}
