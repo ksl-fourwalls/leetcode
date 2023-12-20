8962527e77519e4b1990b9a4ff78801347de0ebf
user <user@user-Lenovo-V14-ADA.com>
2023-12-20 18:25:06 +0530

    rotate won't work

---
+++ maximum_product_difference.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// product difference between two pairs (a,b) and (c,d) is 
+// defined as (a*b)-(c*d)
+
+class Solution {
+public:
+	int maxProductDifference(vector<int>& nums) {
+		return -1;
+	
+};
+
+int main(int argc, char* argv[]) {
+	Solution s;
+	vector<int> nums;
+	cout << s.maxProductDifference(nums) << endl;
+	return 0;
+}
---
+++ rotate_array.cpp
@@ -0,0 +1,16 @@
+// 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	void rotate(vector<int>& nums, int k) {
+
+	}
+};
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
